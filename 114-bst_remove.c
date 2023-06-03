dfb464165a2247227ad698272fef568e049cb11d
Biggiesmall67 <isaacike67@gmail.com>
2023-06-02 17:34:03 -0700

    best remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -78,4 +78,22 @@ bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value)
 			return (bst_delete(root, node));
 		if (node->n > value)
 			return (bst_remove_recursive(root, node->left, value));
+		return (bst_remove_recursive(root, node->right, value));
+	}
+	return (NULL);
+}
 
+/**
+ * bst_remove - Removes a node from a binary search tree.
+ * @root: A pointer to the root node of the BST to remove a node from.
+ * @value: The value to remove in the BST.
+ *
+ * Return: A pointer to the new root node after deletion.
+ *
+ * Description: If the node to be deleted has two children, it
+ *              is replaced with its first in-order successor.
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	return (bst_remove_recursive(root, root, value));
+}
